0c729c63f2c5c1f4e366433cbd9861cbd3e09848
Manuel Duarte  <carlosd99m@gmail.com>
2022-12-13 21:20:33 -0600

    chore(deps): upgrade flutter version (#7)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-import 'src/my_app.dart';
+import 'app/app.dart';
 
 void main() async {
   runApp(MyApp());
